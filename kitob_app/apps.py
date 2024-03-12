7d68e7a4e0f6fcd32cf4831d977636366977c892
User <tjasur@gmail.com>
2024-03-12 10:59:30 +0500

    add: model, serializers

---
+++ kitob_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KitobAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'kitob_app'
---
+++ kitob_app/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email is required')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
